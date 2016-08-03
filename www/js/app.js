bd8967185216a0585d577146825e1bfb9307cd68
Limo Saplf <LimoSaplf@outlook.com>
2016-08-03 10:06:01 +0800

    tabs init

--- www/js/app.js
+++ www/js/app.js
@@ -5,57 +5,57 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('chat', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
 
-.config(['', '', function ($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('tabs', {
-      url: '/tabs',
-      abstracts: true,
-      templateUrl: 'templates/tabs.html',
-      controller: 'TabCtrl'
-    })
-    .state('tabs.session',{
-      url:'/session',
-      views:{
-        'tab-content':{
-          templateUrl:'templates/tab-session.html',
-          controller:'SessionCtrl'
+  .config(['', '', function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('tabs', {
+        url: '/tabs',
+        abstracts: true,
+        templateUrl: 'templates/tabs.html',
+        controller: 'TabCtrl'
+      })
+      .state('tabs.session', {
+        url: '/session',
+        views: {
+          'tab-session': {
+            templateUrl: 'templates/tab-session.html',
+            controller: 'SessionCtrl'
+          }
         }
-      }
-    })
-    .state('tabs.contact',{
-      url:'/contact',
-      views:{
-        'tab-content':{
-          templateUrl:'templates/tab-contact.html',
-          controller:'ContactCtrl'
+      })
+      .state('tabs.contact', {
+        url: '/contact',
+        views: {
+          'tab-contact': {
+            templateUrl: 'templates/tab-contact.html',
+            controller: 'ContactCtrl'
+          }
         }
-      }
-    })
-    .state('tabs.apply',{
-      url:'/apply',
-      views:{
-        'tab-content':{
-          templateUrl:'templates/tab-apply.html',
-          controller:'ApplyCtrl'
+      })
+      .state('tabs.apply', {
+        url: '/apply',
+        views: {
+          'tab-apply': {
+            templateUrl: 'templates/tab-apply.html',
+            controller: 'ApplyCtrl'
+          }
         }
-      }
-    })
-}]);
+      })
+  }]);
