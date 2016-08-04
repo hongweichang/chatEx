5dc33b1ab5be40e3ecd4f895f5f902f6621b38fe
macro <1300243672@qq.com>
2016-08-04 13:55:44 +0800

    item  、图标颜色处理

--- www/js/controller/tabContactController.js
+++ www/js/controller/tabContactController.js
@@ -3,5 +3,12 @@
 angular.module('ChatApp')
 
   .controller('tabContactCtrl', ['$scope', function ($scope) {
-
+    $scope.hide1=true;
+    $scope.hide2=true;
+    $scope.show=function(index){
+     switch(index){
+       case 1:$scope.hide1=!$scope.hide1;break;
+       case 2:$scope.hide2=!$scope.hide2;break;
+     }
+    };
   }]);
