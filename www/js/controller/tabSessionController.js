3ab22f8a54438214c2c204418a72e8a30f9b469f
Limo Saplf <LimoSaplf@outlook.com>
2016-08-04 13:59:50 +0800

    ...

--- www/js/controller/tabSessionController.js
+++ www/js/controller/tabSessionController.js
@@ -3,5 +3,13 @@
 angular.module('ChatApp')
 
 .controller('tabSessionCtrl', ['$scope', function ($scope) {
-
+  $scope.isScroll = true;
+  $scope.$on('horStart', function () {
+    console.log('horStart');
+    $scope.isScroll = false;
+  });
+  $scope.$on('horEnd', function () {
+    console.log('horEnd');
+    $scope.isScroll = true;
+  })
 }]);
--- www/js/directive/common.js
+++ www/js/directive/common.js
@@ -1,3 +1,8 @@
+/**
+ * @author saplf
+ * @date 2016/8/4
+ * @description definition for directives
+ */
 'use strict';
 
 angular.module('ChatApp')
@@ -18,10 +23,37 @@ angular.module('ChatApp')
       chatRightCallback: '=chatRightCallback' // 右侧 触发函数
     },
     controller: ['$scope', function ($scope) {
+      $scope.leftIsText = !!$scope.chatRight;
       $scope.leftIsAvator = !!$scope.chatSrc;
+      $scope.rightIsText = !!$scope.chatLeft;
       $scope.rightIsIcon = !!$scope.chatRightIcon;
       $scope.left = $scope.chatLeftCallback || function(){};
       $scope.right = $scope.chatRightCallback || function(){};
+    }]
+  }
+})
+
+.directive('chatListItem', function () {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'templates/directive/chat-list-item.html',
+    scope: {
+      chatAvator: '=chatAvator'
+    },
+    controller: ['$scope', '$ionicGesture', function ($scope, $ionicGesture) {
+      var ele = angular.element(document.querySelectorAll('.chat-list-item'));
+      $ionicGesture.on('dragstart', function () {
+        $scope.$emit('horStart');
+      }, ele);
+      $ionicGesture.on('dragleft', function (e) {
+        this.style.transform = ['translateX(-', e.gesture.distance, 'px)'].join('');
+      }, ele);
+      $ionicGesture.on('dragend', function () {
+        $scope.$emit('horEnd');
+        this.style.transform = 'translateX(0)';
+        this.style.transition = 'transform .3s';
+      }, ele)
     }]
   }
 });
